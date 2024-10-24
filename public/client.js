d5627e485df1ea37a600f62718fdca17715a418b
Martin Janiczek <martin@janiczek.cz>
2024-10-24 18:36:27 +0200

    Prompt for IP

--- public/client.js
+++ public/client.js
@@ -1,12 +1,14 @@
-const serverAddress = window.location.hostname;
+const abletonServerAddress = window.location.hostname;
 
-document.getElementById('lyricslides-iframe').src = `http://${serverAddress}:5115`;
+const lyricslidesServerAddress = prompt("LyricSlides IP", abletonServerAddress);
+
+document.getElementById('lyricslides-iframe').src = `http://${abletonServerAddress}:5115`;
 
 const app = Elm.Main.init({
     node: document.getElementById('elm'),
 });
 
-const socket = new WebSocket(`ws://${serverAddress}:8001`);
+const socket = new WebSocket(`ws://${abletonServerAddress}:8001`);
 
 socket.onmessage = (event) => {
     app.ports.onWebsocketMessage.send(event.data);
